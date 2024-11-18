8d4be732abf2a12d1e6cff816ba00030e3c60af0
JonhNino <jonhninoa@gmail.com>
2024-11-17 21:21:50 -0500

    Crud

---
+++ .jetclient/jetclient.md
@@ -0,0 +1,10 @@
+```toml
+name = 'Crud'
+id = '3ebff47c-37f3-42d5-95ab-a8dcebc7d5b8'
+
+[[environmentGroups]]
+name = 'Default'
+
+[[apis]]
+name = 'API'
+```
--- src/main/java/com/quarkus/crud/GreetingResource.java
+++ src/main/java/com/quarkus/crud/GreetingResource.java
@@ -1,16 +1,34 @@
 package com.quarkus.crud;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/crud")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class GreetingResource {
 
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public Response getAll() {
+        return Response.ok("GET response: All items retrieved").build();
+    }
+
+    @POST
+    public Response create(String item) {
+        return Response.ok("POST response: Created item " + item).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response update(@PathParam("id") String id, String item) {
+        return Response.ok("PUT response: Updated item " + id + " with " + item).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response delete(@PathParam("id") String id) {
+        return Response.ok("DELETE response: Deleted item with id " + id).build();
     }
 }
